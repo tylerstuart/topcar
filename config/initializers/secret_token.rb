198bbbc44e737bc2d03d87e06ed94af69f14de5e
Tyler Stuart <tstuart@1on1.com>
2012-10-28 16:21:51 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Topcar::Application.config.secret_token = '5eb287a59d123411110053b03d70911a6814110bfc993d52cf8ad048bedc7e04b794bf9444e40681580e763532cf29c2e720fa43a3f0c268c7652ebc3213bc77'
